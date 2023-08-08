981d1886c070357d0c2fb3c383f3332c58a69529
Yukta Goel <yukta.goyal25@gmail.com>
2023-08-08 23:55:03 +0530

    update: import data from constants

--- src/App.jsx
+++ src/App.jsx
@@ -1,120 +1,22 @@
 import './App.css';
-
-const resdata = {
-    "id": "23742",
-    "name": "McDonald's",
-    "cloudinaryImageId": "ee5f8e06b300efc07c9fe3f4df40dfc4",
-    "locality": "Chennai Kilpauk",
-    "areaName": "Kilpauk",
-    "costForTwo": "₹400",
-    "cuisines": [
-      "Burgers",
-      "Beverages",
-      "Cafe",
-      "Desserts"
-    ],
-    "avgRating": 4.2,
-    "feeDetails": {
-      "restaurantId": "23742",
-      "fees": [
-        {
-          "name": "BASE_DISTANCE",
-          "fee": 4700
-        },
-        {
-          "name": "BASE_TIME"
-        },
-        {
-          "name": "ANCILLARY_SURGE_FEE"
-        }
-      ],
-      "totalFee": 4700
-    },
-    "parentId": "630",
-    "avgRatingString": "4.2",
-    "totalRatingsString": "10K+",
-    "sla": {
-      "deliveryTime": 22,
-      "lastMileTravel": 4.6,
-      "serviceability": "SERVICEABLE",
-      "slaString": "22 mins",
-      "lastMileTravelString": "4.6 km",
-      "iconType": "ICON_TYPE_EMPTY"
-    },
-    "availability": {
-      "nextCloseTime": "2023-08-08 02:45:00",
-      "opened": true
-    },
-    "badges": {
-      
-    },
-    "isOpen": true,
-    "type": "F",
-    "badgesV2": {
-      "entityBadges": {
-        "imageBased": {
-          
-        },
-        "textBased": {
-          
-        },
-        "textExtendedBadges": {
-          
-        }
-      }
-    },
-    "loyaltyDiscoverPresentationInfo": {
-      "logoCtx": {
-        "text": "BENEFITS",
-        "logo": "v1634558776/swiggy_one/OneLogo_3x.png"
-      },
-      "freedelMessage": "FREE DELIVERY"
-    },
-    "orderabilityCommunication": {
-      "title": {
-        
-      },
-      "subTitle": {
-        
-      },
-      "message": {
-        
-      },
-      "customIcon": {
-        
-      }
-    },
-    "differentiatedUi": {
-      "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
-      "differentiatedUiMediaDetails": {
-        "mediaType": "ADS_MEDIA_ENUM_IMAGE",
-        "lottie": {
-          
-        },
-        "video": {
-          
-        }
-      }
-    },
-    "reviewsSummary": {
-      
-    },
-    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
-    "restaurantOfferPresentationInfo": {
-      
-    }
-}
+import React from 'react';
+import { resdata } from './constants/restaurant';
 
 const Search = () => {
   return <div className='p-2.5'>SearchBar</div>
 }
 const RestroCard = ({resdata}) => {
-  return <div className='flex flex-col w-[250px] h-[430px] hover:border-2 cursor-pointer bg-slate-300 p-4 m-2 gap-2'>
-    <img src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" + resdata.cloudinaryImageId} alt='biryani'></img>
-    <div className='text-xl font-medium'>{resdata.name}</div>
-    <div className='text-sm font-extralight'>{resdata.cuisines.join(',')}</div>
-    <div className='text-xl font-light'>{resdata.costForTwo}</div>
-    <div className='text-xl font-light'>{resdata.avgRating}</div>
+  return <div className='flex flex-col w-64 hover:border-2 cursor-pointer bg-slate-300 p-4 m-2 gap-2'>
+      <img
+        src={
+          "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/" +
+         resdata.data.cloudinaryImageId
+        } alt={'food'}
+      ></img>
+    <div className='text-xl font-medium'>{resdata.data.name}</div>
+    <div className='text-sm font-extralight truncate'>{resdata.data.cuisines.join(',')}</div>
+    <div className='text-xl font-light'>Rs. {resdata.data.costForTwo/100}</div>
+    <div className='text-xl font-light'>Rating: {resdata.data.avgRating}</div>
   </div>
 }
 
@@ -123,7 +25,9 @@ const Body = () => {
     <div>
       <Search/>
       <div className='flex flex-wrap'>
-      <RestroCard resdata={resdata}/>
+        {resdata.map((restaurant)=>{
+          return <RestroCard key={restaurant.data.id} resdata={restaurant}/>
+        })}
       </div>
     </div>
   )
@@ -131,7 +35,7 @@ const Body = () => {
 
 const Header = () => {
   return <div className='flex justify-between border-2'>
-    <div className='h-22 w-28'>
+    <div className='h-20 w-28'>
       <img src='https://penji.co/wp-content/uploads/2022/08/11.Foodigy-logo.jpg' alt='logo' />
       </div> 
       <div className='px-10 py-8'>
@@ -143,8 +47,6 @@ const Header = () => {
       </div>
       </div>
 }
-
-
 
 
 const AppLayout = () => {
