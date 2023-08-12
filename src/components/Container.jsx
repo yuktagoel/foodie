1d500298173d082a63328245020cd4fc52e6b080
Yukta Goel <yukta.goyal25@gmail.com>
2023-08-12 22:47:29 +0530

    fix: no render

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -5,35 +5,50 @@ import { useEffect, useState } from "react";
 // import Filter from "./Filter";
 
 const Container = () => {
-  const handleClick = () => {
-     setdata(data.filter((res)=>{
-         return res.info.avgRating > 4.2;
-        }))
-  }
-  const fetchData = async () =>{
-    const dataa = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.0826802&lng=80.2707184&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
-    const json = await dataa.json();
-    setdata(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
+
+  const [cards, setCards] = useState([]);
+
+  // const handleClick = () => {
+  //   setCards(
+  //     data.filter((res) => {
+  //       return res.info.avgRating > 4.2;
+  //     })
+  //   );
+  // };
+  const fetchData = async () => {
+    const req = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.0826802&lng=80.2707184&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await req.json();
+    setCards(
+      json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants
+    );
   };
-  
-    useEffect(()=>{
-      fetchData();
-    },[]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
 
-
-  const [data , setdata] = useState([]);
+  if (cards?.length === 0) {
+    return <div>Loading...</div>;
+  }
   return (
     <div>
       <div className="flex justify-between">
-      <button className='bg-blue-200 h-12 w-24' onClick={handleClick}>TopRated</button>
+        {/* <button className="bg-blue-200 h-12 w-24" onClick={handleClick}> */}
+          {/* TopRated
+        </button> */}
         <Search />
       </div>
 
       <div className="flex flex-wrap">
-        {data.map((restaurant) => {
-          return <RestroCard key={restaurant.info.id} resdata={restaurant.info} />;
-        })} 
+        {cards &&
+          cards.map((restaurant) => {
+            return (
+              <RestroCard key={restaurant.info.id} resdata={restaurant.info} />
+            );
+          })}
       </div>
     </div>
   );
