9da94fef935715e52fc62584212a61a9c7099ff2
Yukta Goel <yukta.goyal25@gmail.com>
2023-08-16 00:56:02 +0530

    add: good nav

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import  About  from "./components/header-pages/About";
+import AppLayout from "./App";
+import ContactUs from "./components/header-pages/ContactUs";
+import ErrorPage from "./components/error-page/ErrorPage";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <AppLayout />,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: "/about",
+      element: <About />,
+    },
+    {
+      path: "/contact",
+      element: <ContactUs />,
+    }
+  ]);
+  
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(<RouterProvider router={appRouter} />);
+
