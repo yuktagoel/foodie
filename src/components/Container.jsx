7a022737d603d488eb350e0d3948de1e05fe3484
Yukta Goel <yukta.goyal25@gmail.com>
2023-09-18 22:17:01 +0530

    revert: container changes

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,29 +1,44 @@
 import RestroCard from "./RestroCard";
+// import { resdata } from "../utils/restaurant";
 import Search from "./Search";
 import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
 import { Link } from "react-router-dom";
-import useFetchCardData from "./hooks/useFetchCardData";
+// import Filter from "./Filter";
 
 const Container = () => {
+  const [cards, setCards] = useState([]);
   const [searchedCards, setSearchedCards] = useState([]);
   const [search, setSearch] = useState("");
   const searchIconHtml = "&#128269";
   const handleClick = () => {
-    searchedCards?.length > 0 && console.log("changing");
-    console.log(searchedCards);
+    cards?.length > 0 && console.log("changing");
+    console.log(cards);
     setSearchedCards(
-      searchedCards.filter((card) => {
+      cards.filter((card) => {
         return card.info.avgRating >= 4.5;
       })
     );
   };
 
-  const cards = useFetchCardData();
+  const fetchData = async () => {
+    console.log("fetching");
+    const req = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await req.json();
+    console.log(json.data);
+    setCards(
+      json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setSearchedCards(
+      json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
 
   useEffect(() => {
-    setSearchedCards(cards);
-  }, [cards]);
+    fetchData();
+  }, []);
 
   return cards?.length === 0 || !cards ? (
     <Shimmer>Loading</Shimmer>
@@ -56,7 +71,6 @@ const Container = () => {
           </button>
         </div>
       </div>
-
       <div className="flex flex-wrap">
         {searchedCards &&
           searchedCards.map((restaurant) => {
@@ -76,5 +90,4 @@ const Container = () => {
     </div>
   );
 };
-
 export default Container;
